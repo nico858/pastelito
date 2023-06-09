37ac8cb748ce5799535c6214dcf84c2b391afccd
nico858 <nicolasurbanos10@gmail.com>
2023-06-09 06:35:56 -0500

    Documentation finished

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,3 +1,90 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: The auth managing API
+ * /auth/login:
+ *   post:
+ *     summary: Login for the user
+ *     tags: [Auth]
+ *     requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:  
+ *                email:
+ *                  type: string
+ *                password:
+ *                  type: string
+ *     responses:
+ *       200:
+ *         description: The list of the users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 email:
+ *                  type: string
+ *                 password:
+ *                  type: string
+ * /auth/recovery:
+ *   post:
+ *     summary: Ask for a recovery token
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *              email:
+ *                type: string 
+ *     responses:
+ *       200:
+ *         description: The generated token.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                token:
+ *                  type: string
+ *       500:
+ *         description: Server error
+ * /auth/change-password:
+ *   post:
+ *     summary: Change the password with the token
+ *     tags: [Auth]
+ *     request body:
+ *      required: true
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         properties:
+ *          token:
+ *           type: string
+ *          newPassword:
+ *            type: string
+ *     responses:
+ *       200:
+ *         description: Passowrd changed succesfully
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                message:
+ *                 type: string
+ *       404:
+ *         description: The password was not valid
+ */
+
+
 import express from 'express';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
